930f77d266435a7447ca8fef7a5c2e0dea24a157
alazar-da <alazar.damena01@gmail.com>
2025-11-24 16:13:57 +0300

    fixed

--- google-auth.ts
+++ google-auth.ts
@@ -8,7 +8,7 @@ export function initGoogleAuth() {
   }
 
   GoogleAuth.initialize({
-    clientId: "576182804983-tn4sariou6g144584e79aaglbk6lbabp.apps.googleusercontent.com",
+    clientId: "576182804983-8vdsir74pstcvd4leifn2s76b43b0sr9.apps.googleusercontent.com",
     scopes: ["profile", "email"],
     grantOfflineAccess: true,
   });
