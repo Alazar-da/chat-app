9f4809422047898bd932f2f44328a639a943c198
alazar-da <alazar.damena01@gmail.com>
2025-12-07 21:22:36 +0300

    mobile design edited

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ export const metadata = { title: "Chat App" };
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className="safe-area">
+      <body className="pb-[40px]">
         <AuthProvider>
           {children}
         </AuthProvider>
